49437f8535bd09f1dccf778386e999c86a43f1e7
adamdevigili <adam.devigili@gmail.com>
2017-02-09 14:44:28 -0600

    Added edge cases and logging

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 # -*- coding: utf-8 -*-
-from PyDictionary import PyDictionary
 import requests
 import json
+import logging
 
-def getLongestSyn(word):
+LOG_FILENAME = 'TheSaurus.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+
+def TheSaurus():
+    usedWords = []
+    filedata = None
+    minWordLength = 5
+
+    newString = ""
+    with open('data.txt', 'r') as file:
+      filedata = file.read()
+
+    for word in filedata.split():
+        if len(word) >= minWordLength:
+            newSyn = str(getLongestSyn(word, usedWords))
+            if newSyn != "":            #If getLongestSyn finds a new synonym, append to new string. Otherwise, append the original word
+                newString += newSyn
+                usedWords.append(newSyn)
+            else:
+                newString += word
+        else:
+            newString += str(word)
+        newString += " "
+
+    print newString
+
+def getLongestSyn(word, usedWords):
     api_url = "http://pydictionary-geekpradd.rhcloud.com/api/synonym/"
     longest = ""
 
-    response = requests.get(api_url + word)
+    response = requests.get(api_url + word)     #Query API for synonyms
 
     if(response.ok):
         syns = json.loads(response.content)
+        logging.debug(word + ": " + str(syns))
+
         for syn in syns:
             if len(syn) > len(longest):
-                longest = syn
+                if syn not in usedWords:
+                    longest = syn
             print longest + ", " + syn
     else:
         print "Bad things happened"
+
     return longest
 
-usedWords = []
-filedata = None
-dictionary = PyDictionary()
-minWordLength = 5
-
-newString = ""
-with open('data.txt', 'r') as file:
-  filedata = file.read()
-
-for word in filedata.split():
-    if len(word) >= minWordLength:
-        newString += str(getLongestSyn(word))
-    else:
-        newString += str(word)
-    newString += " "
-    
-print newString
+if __name__ == "__main__":
+    TheSaurus()
